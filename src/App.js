00dc8ad74886a9ad56f8f97311cc81d649bbd1ab
Doyoon Kim <asfrom30@gmail.com>
2021-07-25 14:01:57 +0900

    리액트 라우터 추가 및 기본 페이지 생성

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Route } from 'react-router-dom';
 
-function App() {
+import { Home, AvatarComposition, ItemSimulator } from './pages';
+
+function App(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Route exact path="/" component={Home} />
+      <Route path="/item" component={ItemSimulator} />
+      <Route path="/avatar" component={AvatarComposition} />
     </div>
   );
 }
---
+++ src/pages/AvatarComposition.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function AvatarComposition(props) {
+  return <div>avatar composition</div>;
+}
+
+export default AvatarComposition;
