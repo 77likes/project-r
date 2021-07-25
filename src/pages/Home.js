0204ca4eaa24d8f836074504482564583030edc7
Doyoon Kim <asfrom30@gmail.com>
2021-07-25 14:03:49 +0900

    Home에 각 페이지 링크 이동 리스트 구현

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,23 @@
 import React from 'react';
 
+import { Link } from 'react-router-dom';
+
 function Home(props) {
-  return <div>home</div>;
+  return (
+    <div>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/item">Item</Link>
+        </li>
+        <li>
+          <Link to="/avatar">Avatar</Link>
+        </li>
+      </ul>
+    </div>
+  );
 }
 
 export default Home;
