af0a011af32037c3c5d0eebea6508b7ed5ec2932
winterguard <aliasbonfire@gmail.com>
2021-08-04 20:52:47 +0900

    add browser router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 import { Home, AvatarComposition, ItemSimulator } from './pages';
 
 function App(props) {
   return (
     <div>
-      <Route exact path="/" component={Home} />
-      <Route path="/item" component={ItemSimulator} />
-      <Route path="/avatar" component={AvatarComposition} />
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
+        <Route exact path="/" component={Home} />
+        <Route path="/item" component={ItemSimulator} />
+        <Route path="/avatar" component={AvatarComposition} />
+      </BrowserRouter>
     </div>
   );
 }
