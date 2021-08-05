e073c3d10f58695e3a40a1cf8f6d5c2f56ecab1e
winterguard <aliasbonfire@gmail.com>
2021-08-05 23:15:07 +0900

    set hashRouter

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { HashRouter, Route } from 'react-router-dom';
 
 import { Home, AvatarComposition, ItemSimulator } from './pages';
 
 function App(props) {
   return (
     <div>
-      <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <HashRouter basename={process.env.PUBLIC_URL}>
         <Route exact path="/" component={Home} />
         <Route path="/item" component={ItemSimulator} />
         <Route path="/avatar" component={AvatarComposition} />
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 }
